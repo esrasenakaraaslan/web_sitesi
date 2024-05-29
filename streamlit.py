05f9b9a88385a49c75983af3cf60eae2c593ff77
esrasenakaraaslan <147700240+esrasenakaraaslan@users.noreply.github.com>
2024-05-29 13:17:03 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write("merhaba")
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Sütun 1")
+    st.write("Bu sütun 1'deki içerik.")
+with col2:
+    st.header("Sütun 2")
+    st.write("Bu sütun 2'deki içerik.")
