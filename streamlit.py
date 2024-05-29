f514a4fe6206fee666d1cc3abe2fa84ed82f3c7c
esrasenakaraaslan <147700240+esrasenakaraaslan@users.noreply.github.com>
2024-05-29 15:22:54 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-# Arka plan rengini belirleyen stil
+# Arka plan rengini ve yazı stilini belirleyen stil
 page_bg_img = '''
 <style>
 body {
 background-color: #FF00FF;
+color: white;
 }
 </style>
 '''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
