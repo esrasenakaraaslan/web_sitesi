087da015dc34574df2372112215a222eeda6f8f5
esrasenakaraaslan <147700240+esrasenakaraaslan@users.noreply.github.com>
2024-06-05 21:56:03 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,64 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Uygulama ayarları
 st.set_page_config(page_title="FreshData", page_icon=":rocket:", layout="wide")
 
+# Özel CSS ile stil ayarları
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #aed6f1; /* Tatlı mavi */
+    }
+    .stButton>button {
+        color: #ffffff; /* Beyaz */
+        background-color: #9b59b6; /* Mor */
+        border: none;
+        padding: 10px 24px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 12px;
+        transition-duration: 0.4s;
+    }
+    .stButton>button:hover {
+        background-color: #8e44ad; /* Koyu mor */
+    }
+    .header-title {
+        color: #9b59b6; /* Mor */
+        font-family: 'Arial', sans-serif;
+        text-align: center;
+        margin-top: 20px;
+    }
+    .info-box {
+        background-color: #ec407a; /* Pembe */
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        margin-bottom: 20px;
+        font-family: 'Arial', sans-serif;
+    }
+    .info-box p {
+        margin: 0;
+    }
+    .footer {
+        text-align: center;
+        margin-top: 50px;
+        font-size: 12px;
+        color: #888;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Başlık
-st.markdown('<h1 style="color: #9b59b6; text-align: center;">FreshData İş İlanı Sitesi</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="header-title">FreshData İş İlanı Sitesi</h1>', unsafe_allow_html=True)
 
 # URL
 url = "https://raw.githubusercontent.com/esrasenakaraaslan/datasetim/main/t%C3%BCm_veriler_d%C3%BCzenlenmi%C5%9F_y%C4%B1ll%C4%B1.xlsx" 
@@ -15,51 +68,50 @@ url = "https://raw.githubusercontent.com/esrasenakaraaslan/datasetim/main/t%C3%B
 def load_data(url): 
     return pd.read_excel(url)
 
-# Arka plan rengi ve site ismi rengi
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #aed6f1; /* Arka plan rengi */
-    }
-    .stButton>button {
-        color: #f4d03f; /* Buton içindeki yazı rengi (sarı) */
-        background-color: #9b59b6; /* Buton rengi (mor) */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# İşlev butonları ve içerikleri
+# Butonlar ve işlevleri
 if st.button("İş Bul", key="iş_bul_button"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada iş bulma işlevi gelecek.</p></div>', unsafe_allow_html=True)
+    st.markdown('<div class="info-box"><p>Burada iş bulma işlevi gelecek.</p></div>', unsafe_allow_html=True)
 
 if st.button("Meslek Grupları", key="meslek_grupları_button"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada meslek gruplarına göre iş arama işlevi gelecek.</p></div>', unsafe_allow_html=True)
+    st.markdown('<div class="info-box"><p>Burada meslek gruplarına göre iş arama işlevi gelecek.</p></div>', unsafe_allow_html=True)
 
 if st.button("Türkiye'nin Geldiği Son Nokta", key="son_nokta_button"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada Türkiye\'nin geldiği son noktayla ilgili bilgiler yer alacak.</p></div>', unsafe_allow_html=True)
+    st.markdown('<div class="info-box"><p>Burada Türkiye\'nin geldiği son noktayla ilgili bilgiler yer alacak.</p></div>', unsafe_allow_html=True)
 
 if st.button("Analiz"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada veri analizi işlevi gelecek.</p></div>', unsafe_allow_html=True)
+    st.markdown('<div class="info-box"><p>Burada veri analizi işlevi gelecek.</p></div>', unsafe_allow_html=True)
 
 if st.button("Grafikler"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada grafikler çizme işlevi gelecek.</p></div>', unsafe_allow_html=True)
-    
+    st.markdown('<div class="info-box"><p>Burada grafikler çizme işlevi gelecek.</p></div>', unsafe_allow_html=True)
+    # Grafik çizme işlevi buraya eklenecek
+    data = load_data(url)
+    top_locations = data['Konum'].value_counts().head(5)
+    fig, ax = plt.subplots()
+    top_locations.plot(kind='bar', ax=ax, color='#9b59b6')
+    ax.set_xlabel('Konum')
+    ax.set_ylabel('Frekans')
+    ax.set_title('En Çok Tekrar Eden 5 Konum')
+    st.pyplot(fig)
+
 if st.button("İşveren Girişi", key="isveren_girisi_button"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><p style="color: #f4d03f;">Burada işveren giriş işlevi gelecek.</p></div>', unsafe_allow_html=True)
+    st.markdown('<div class="info-box"><p>Burada işveren giriş işlevi gelecek.</p></div>', unsafe_allow_html=True)
 
 # Makaleler bölümü
-st.markdown('<h2 style="color: #9b59b6; text-align: center;">Makaleler</h2>', unsafe_allow_html=True)
+st.markdown('<h2 class="header-title">Makaleler</h2>', unsafe_allow_html=True)
 
-# Makale 1
 if st.button("Makale 1"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><h3 style="color: #f4d03f;">Başlık 1</h3><p style="color: #f4d03f;">Burada makale içeriği yer alacak.</p></div>', unsafe_allow_html=True)
+    st.markdown('''
+    ## Bilişim Sektöründeki İstihdam Analizi
 
-# Makale 2
+    ### Giriş
+    ...
+    ''')
+
 if st.button("Makale 2"):
-    st.markdown('<div style="background-color: #9b59b6; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"><h3 style="color: #f4d03f;">Başlık 2</h3><p style="color: #f4d03f;">Burada makale içeriği yer alacak.</p></div>', unsafe_allow_html=True)
+    st.markdown('<div class="info-box" style="background-color: #9b59b6;"><h3 style="color: #ec407a;">Başlık 2</h3><p>Burada makale içeriği yer alacak.</p></div>', unsafe_allow_html=True)
+
+if st.button("Makale 3"):
+    st.markdown('<div class="info-box" style="background-color: #9b59b6;"><h3 style="color: #ec407a;">Başlık 3</h3><p>Burada makale içeriği yer alacak.</p></div>', unsafe_allow_html=True)
 
 # Hakkımızda bölümü
 if st.button("Hakkımızda"):
@@ -67,6 +119,6 @@ if st.button("Hakkımızda"):
     ## Bilişim Sektöründe Gelecek: Veri Analizi ve İş İlanları
     ...
     ''')
+    # Footer
+st.markdown('<p class="footer">© 2024 FreshData. Tüm hakları saklıdır.</p>', unsafe_allow_html=True)
 
-# Footer
-st.markdown('<p style="text-align: center; font-size: 12px; color: #888;">© 2024 FreshData. Tüm hakları saklıdır.</p>', unsafe_allow_html=True)
